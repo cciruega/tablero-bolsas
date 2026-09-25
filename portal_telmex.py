e417644a216e1bd3e1aee9701b4a06249502f24c
cciruega <cciruega@gmail.com>
2026-09-25 16:57:51 -0600

    Update portal_telmex.py

--- portal_telmex.py
+++ portal_telmex.py
@@ -859,9 +859,7 @@ if archivo_a_procesar is not None:
                     st.table(estilo_resaltado(aplicar_subtotales(td_b69_canal)))
 
                     # Botón de descarga unificado para ambas vistas
-                    generar_boton_descarga(df_b69_cp, 'folios_t5_bolsa69', btn_key='btn_com_bolsa_69')
-                else: 
-                    st.info("No hay datos en la Bolsa 6.9 para mostrar.")
+                    generar_boton_descarga(df_b69_cp, 'folios_t5_bolsa69', btn_key='btn_com_bolsa_69') 
 
                 # TABLA C: Nueva Vista (FIELDERS por Empresa Externa)
                     st.divider()
